42bd7cf38189755fe52e78fa072ff788b30de3df
Anikesh Roy <anikeshroy62040@gmail.com>
2026-01-24 12:30:27 +0530

    Fixed Authentication Bug

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -1,5 +1,5 @@
 const API_CONFIG = {
-    BASE_URL: 'http://127.0.0.1:5000/api',
+    BASE_URL: 'https://fastsewawebsite-production.up.railway.app/api',
     ENDPOINTS: {
         REGISTER: '/auth/register',
         LOGIN: '/auth/login',
